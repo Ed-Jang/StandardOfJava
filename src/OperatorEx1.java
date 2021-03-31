f65ac6057a921645a5e02c71b86555b7559191ad
82106 <82106@DESKTOP-2D8OQ7G>
2021-03-31 19:25:18 +0900

    Operator ex

---
+++ src/OperatorEx1.java
@@ -0,0 +1,12 @@
+
+class OperatorEx1 {
+	public static void main(String args[]) {
+		int i = 5;
+		i++; //i=i+1과 같은 의미
+		System.out.println(i);
+		i=5; //결과비교를 위해 i값을 다시 5로 변경
+		++i;
+		System.out.println(i);
+		
+	}
+}
