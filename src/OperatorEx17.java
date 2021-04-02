6c2c9aee1a102eada290ed3f91212a9c353f42df
Ed-Jang <boreding@naver.com>
2021-04-02 20:35:00 +0900

    add operator ex division, mod

---
+++ src/OperatorEx17.java
@@ -0,0 +1,9 @@
+
+class OperatorEx17 {
+	public static void main(String[] args) {
+		double pi = 3.141592;
+		double shortPi = (int) (pi * 1000 + 0.5) / 1000.0;//0.5를 더해서 반올림
+
+		System.out.println(shortPi);
+	}
+}
---
+++ src/OperatorEx18.java
@@ -0,0 +1,8 @@
+
+class OperatorEx18 {
+	public static void main(String[] args) {
+		double pi = 3.141592;
+		double shortPi = Math.round(pi * 1000) / 1000.0; //round메서드는 소수점 첫째자리에서 반올림하고 정수 돌려줌
+		System.out.println(shortPi);
+	}
+}
