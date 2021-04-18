99d1ec5792d919245853d1123512f79cf435c9a8
Ed-Jang <boreding@naver.com>
2021-04-18 16:03:15 +0900

    Add Ex

---
+++ src/CallStackTest.java
@@ -0,0 +1,19 @@
+
+class CallStackTest {
+	public static void main(String[] args) {
+		System.out.println("main(String[] args)이 시작되었음");
+		firstMethod(); // static메서드는 객체 생성없이 호출가능
+		System.out.println("main(String[] args)이 끝났음");
+	}
+
+	static void firstMethod() {
+		System.out.println("firstMethod()이 시작되었음");
+		secondMethod();
+		System.out.println("firstMethod()이 끝났음");
+	}
+
+	static void secondMethod() {
+		System.out.println("secondMethod()이 시작되었음");
+		System.out.println("secondMethod()이 끝났음");
+	}
+}
---
+++ src/MainTest.java
@@ -0,0 +1,7 @@
+
+class MainTest {
+	public static void main (String[] args) {
+		main(null); //재귀호출
+	}
+}
+//StackOverflowError발생
