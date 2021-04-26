c5c093f736daa43a866c90fa0c23c7ecc9747411
Ed-Jang <boreding@naver.com>
2021-04-26 23:34:08 +0900

    Add Ex

---
+++ src/FinalCardTest.java
@@ -0,0 +1,30 @@
+class MyCard{
+	final int NUMBER; //상수지만 선언과 함께 초기화 하지 않는다
+	final String KIND; //생성자에서 단 한번만 초기화 가능
+	static int width = 100;
+	static int height = 250;
+	
+	MyCard(String kind, int num){
+		KIND = kind;
+		NUMBER = num;
+	}
+	
+	MyCard(){
+		this("HEART",1);
+	}
+	
+	public String toString() {
+		return KIND + " " + NUMBER;
+	}
+	
+	
+}
+class FinalCardTest {
+	public static void main(String[] args ) {
+		MyCard c = new MyCard("HEART", 10);
+		
+		System.out.println(c.KIND);
+		System.out.println(c.NUMBER);
+		System.out.println(c);
+	}
+}
