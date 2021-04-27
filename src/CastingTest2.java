c83823bb14d8f3ddff1b045b0278d4003cfe137c
Ed-Jang <boreding@naver.com>
2021-04-28 00:53:18 +0900

    Add Ex

--- src/CastingTest2.java
+++ src/CastingTest2.java
@@ -6,7 +6,7 @@ class CastingTest2 {
 		FireEngine fe = null;
 		
 		car.drive();
-		fe = (FireEngine)car;//실행시 에러 
+		fe = (FireEngine)car;//실행시 에러, 조상타입의 인스턴스를 자손타입의 참조변수로 참조하는 것은 안됨
 		fe.drive();
 		car2 = fe;
 		car2.drive();
