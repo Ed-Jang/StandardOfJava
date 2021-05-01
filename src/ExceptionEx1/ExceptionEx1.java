98a3b0f0c0eb38ce297afa30b277bf4810a376b6
Ed-Jang <boreding@naver.com>
2021-05-01 23:22:18 +0900

    Add ex

---
+++ src/ExceptionEx1/ExceptionEx1.java
@@ -0,0 +1,30 @@
+package ExceptionEx1;
+
+class ExceptionEx1 {
+	public static void main(String[] args) {
+		try {
+			try {
+				
+			}
+			catch(Exception e){
+				
+			}
+		}
+		catch(Exception e) {
+			/*
+			 * try {
+			 * 
+			 * } catch(Exception e) {//에러 변수 e가 중복 선언
+			 * 
+			 * }
+			 */
+		}//try catch
+		
+		try {
+			
+		}
+		catch(Exception e) {
+			
+		}
+	}
+}
