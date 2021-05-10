020c8ff3191ad049c3b06e8ed8497d103636e3e1
Ed-Jang <boreding@naver.com>
2021-05-10 12:39:34 +0900

    Add Ex

---
+++ src/RandomEx1/RandomEx1.java
@@ -0,0 +1,23 @@
+package RandomEx1;
+
+import java.util.Random;
+
+class RandomEx1 {
+	public static void main(String[] args) {
+		Random rand = new Random(1);
+		Random rand2 = new Random(1);
+
+		System.out.println("= rand =");
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i + ":" + rand.nextInt());
+		}
+
+		System.out.println();
+		System.out.println("= rand2 =");
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i + ":" + rand2.nextInt());
+		}
+		//Random인스턴스 rand rand2가 같은 종자 값(seed)을 사용하기 때문에 같은 값이 나옴
+	}
+
+}
